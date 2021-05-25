011aed0dd5c6ab160084ab44df03820ba35bae38
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-05-25 18:25:19 +0200

    Address code comments

--- src/Permutation.scala
+++ src/Permutation.scala
@@ -1,31 +1,22 @@
 object Permutation {
-  def permute[A](l: List[A]): LazyList[List[A]] = l match {
-    case Nil => LazyList(List())
-    case _ => for ((x, xs) <- splits(l); perm <- permute(xs)) yield x::perm
-  }
-
-  /**
-   * Returns a stream of all the tuples (a, as) obtained from the list "l"
-   * where "a" is an element of "l" and "as" is the list "l" where "a" was
-   * removed.
-   */
-  def splits[A](l: List[A]): LazyList[(A, List[A])] = l match {
+  // member([X|Xs], X, Xs).
+  // member([X|Xs], E, [X|Ys]) :- member(Xs, E, Ys).
+  def member[A](l: List[A]): LazyList[(A, List[A])] = l match {
     case Nil => LazyList()
-    case x::xs => (splits(xs) map(e => (e._1, x::e._2))) :+ (x, xs)
+    case x::xs => (for((e, ys) <- member(xs)) yield (e, x::ys)) :+ (x,xs)
   }
 
-  def permute2[A](xs: List[A]): LazyList[List[A]] = xs match {
+  // permutation([],[]).
+  // permutation(Xs, [X|Ys]) :- member(Xs, X, Zs), permutation(Zs, Ys).
+  def permutation[A](xs: List[A]): LazyList[List[A]] = xs match {
     case Nil => LazyList(List())
-    case _ => for (x <- xs to LazyList; perm <- permute2(xs diff List(x))) yield x::perm
+    case _ => for ((x, zs) <- member(xs); ys <- permutation(zs)) yield x::ys
   }
 }
 
 object PermutationApp extends App{
   import Permutation._
 
-  println(permute(List(1,2,3)).toList)
-  println(permute2(List(1,2,3)).toList)
-
-  println(permute(1 to 6 toList).size)
-  println(permute2(1 to 6 toList).size)
+  println(permutation(List(1,2,3)).toList)
+  println(permutation(1 to 6 toList).size)
 }
